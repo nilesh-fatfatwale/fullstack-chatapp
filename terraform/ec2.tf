9aab2dd0f7aacf8c35eaca75179f7955d36486b0
nilesh-fatfatwale <fatfatwalenilesh@gmail.com>
2025-11-28 13:18:24 +0530

    added terraform files for eks cluster

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -30,7 +30,8 @@ resource "aws_security_group" "sg" {
       { description = "port 22 allow", from = 22, to = 22, protocol = "tcp", cidr = ["0.0.0.0/0"] },
       { description = "port 80 allow", from = 80, to = 80, protocol = "tcp", cidr = ["0.0.0.0/0"] },
       { description = "port 443 allow", from = 443, to = 443, protocol = "tcp", cidr = ["0.0.0.0/0"] },
-      { description = "port 8080 allow", from = 8080, to = 8080, protocol = "tcp", cidr = ["0.0.0.0/0"] }
+      { description = "port 8080 allow", from = 8080, to = 8080, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+      { description = "port 9000 allow", from = 9000, to = 9000, protocol = "tcp", cidr = ["0.0.0.0/0"] }
     ]
     content {
         description =  ingress.value.description
@@ -53,25 +54,30 @@ resource "aws_security_group" "sg" {
 }
 
 
-resource "aws_instance" "myinstance" {
-  ami                    = data.aws_ami.os_image.id
-  instance_type          = "t2.large"
-  key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id              = module.vpc.public_subnets[0]
+# resource "aws_instance" "myinstance" {
+#   ami                    = data.aws_ami.os_image.id
+#   instance_type          = "t2.large"
+#   key_name               = aws_key_pair.key_pair.key_name
+#   vpc_security_group_ids = [aws_security_group.sg.id]
+#   subnet_id              = module.vpc.public_subnets[0]
 
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp3"
-  }
+#   root_block_device {
+#     volume_size = 30
+#     volume_type = "gp3"
+#   }
 
-  tags = {
-    Name = "jenkins Automate"
-  }
-}
+#   tags = {
+#     Name = "jenkins Automate"
+#   }
+# }
 
 
-resource "aws_eip" "server_ip" {
-  instance = aws_instance.myinstance.id 
-  domain = "vpc"
-}
+# resource "aws_eip" "server_ip" {
+#   instance = aws_instance.myinstance.id 
+#   domain = "vpc"
+# }
+
+# resource "aws_ec2_instance_state" "state" {
+#   instance_id = aws_instance.myinstance.id
+#   state = "stopped"
+# }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,16 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.23.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.95.0"
     }
   }
+
   backend "s3" {
-  bucket         = "tf-state-fullstack-chatapp"
-  key            = "terraform.tfstate"
-  region         = "us-east-1"
-  encrypt        = true
-}
+    bucket  = "tf-state-fullstack-chatapp"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
